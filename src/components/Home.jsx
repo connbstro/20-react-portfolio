700426f674f0569d7d6ce76857b30e424e047726
Connor S <constroh@gmail.com>
2023-05-11 17:35:21 -0500

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-[#0a192f]">
+    <div name="home" className="h-screen w-full bg-[#0f141c]">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0f141c] text-gray-300">
       <div>
         <h1 className=" font-thin text-2xl italic font-serif">CS</h1>
       </div>
