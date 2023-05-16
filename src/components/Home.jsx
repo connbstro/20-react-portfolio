b87904dbe995ecab4bbb8ccf26484adcbd7b8ecc
Connor S <constroh@gmail.com>
2023-05-15 23:15:06 -0500

    deployment issues

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import me from "../assets/me.png";
 import { Link } from "react-scroll";
