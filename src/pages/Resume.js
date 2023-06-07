4eb320c88e4d357fc252fb8eae74b2aa5f435705
Connor S <constroh@gmail.com>
2023-06-07 13:45:07 -0500

    resume added to page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Screen from "../assets/Screenshot 2023-06-07 at 1.30.46 PM.png";
 
 const Resume = () => {
   return (
     <div>
       <Navbar />
-      Resume
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div>
+          {" "}
+          <img
+            src={Screen}
+            alt="my resume"
+            // className="object-scale-down mx-auto rounded-2xl w-2/3"
+          />
+        </div>
+      </div>
     </div>
   );
 };
