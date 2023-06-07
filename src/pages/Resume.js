843ede502ec8ed6f634ca149229935c80e3d72b5
Connor S <constroh@gmail.com>
2023-06-07 13:50:22 -0500

    Resume sizing

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,16 +4,18 @@ import Screen from "../assets/Screenshot 2023-06-07 at 1.30.46 PM.png";
 
 const Resume = () => {
   return (
-    <div>
+    <div className="w-full h-full bg-[#0f141c] text-black-300">
       <Navbar />
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div>
-          {" "}
-          <img
-            src={Screen}
-            alt="my resume"
-            // className="object-scale-down mx-auto rounded-2xl w-2/3"
-          />
+      <div className="">
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+          <div className="flex flex-col justify-center h-full">
+            {" "}
+            <img
+              src={Screen}
+              alt="my resume"
+              className="mx-auto w-full m-20"
+            />
+          </div>
         </div>
       </div>
     </div>
